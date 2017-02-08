f45253bb7cf72270898cb90431eff49a81011d12
JohnJeong123 <frenchofkiss@gmail.com>
2017-02-08 15:24:28 +0900

    fixed typo

--- app/controllers/jusolink_controller.rb
+++ app/controllers/jusolink_controller.rb
@@ -1,4 +1,3 @@
-
 class JusolinkController < ApplicationController
 
   # 링크아이디
@@ -7,9 +6,7 @@ class JusolinkController < ApplicationController
   # 비밀키
   SecretKey = "FjaRgAfVUPvSDHTrdd/uw/dt/Cdo3GgSFKyE1+NQ+bc="
 
-  # 팝빌 연동회원 사업자번호
-
-  # 팝빌 전자명세서 API Service 초기화
+  # 주소링크 API Service 초기화
   JusoService = Jusolink.instance(
     JusolinkController::LinkID,
     JusolinkController::SecretKey
@@ -34,7 +31,7 @@ class JusolinkController < ApplicationController
   end
 
   ##############################################################################
-  # 주소검색 서비스 잔여포인트를 확인합니다.
+  # 주소검색 서비스 검색 단가를 확인합니다.
   ##############################################################################
   def getUnitCost
     begin
@@ -47,12 +44,27 @@ class JusolinkController < ApplicationController
     end
   end
 
+  ##############################################################################
+  # 주소검색
+  ##############################################################################
   def zipcode_search
+
+    # 검색어 있는 경우 API 호출
     if params[:IndexWord].to_s != ''
+
+      # 검색어
       indexWord = params[:IndexWord].to_s
+
+      # 페이지 번호
       pageNum = params[:PageNum].to_s
+
+      # 페이지당 목록 개수
       perPage = 20
+
+      # 차등겁색 끄기
       noSuggest = false
+
+      # 수정 제시어 제시 끄기
       noDiff = false
 
       begin
